70c373c150a8b449d3f4aa6dc42d0285816edfcf
Sean Currey <sean.r.currey@gmail.com>
2024-07-22 22:36:44 -0700

    added vibe search and collaboration

--- app.py
+++ app.py
@@ -26,33 +26,56 @@ def main():
 
     # initialize session state
     if 'step' not in st.session_state:
-        st.session_state.step = 1
+        st.session_state.step = 0
     if 'template' not in st.session_state:
         st.session_state.template = ""
     if 'mode' not in st.session_state:
-        st.session_state.template = "trending"
+        st.session_state.mode = "trending"
     if 'top_text' not in st.session_state:
         st.session_state.top_text = ""
     if 'bottom_text' not in st.session_state:
         st.session_state.bottom_text = ""
+    if 'vibe' not in st.session_state:
+        st.session_state.vibe = ""
 
 
     col1, col2 = st.columns([.6,.4])
 
-    with col1:
-        st.image('img/slide.png', use_column_width='auto')
+    if st.session_state.step == 5:
+        exploded_view()
+    elif st.session_state.step == 6:
+        collab_base()
+    else:
+        with col1:
+            st.image('img/slide.png', use_column_width='auto')
 
-    with col2:
-        meme_maker(st.session_state.step)
+        with col2:
+            meme_maker(st.session_state.step)
 
 # meme maker
 def meme_maker(step):
+    if step == 0:
+        view_select()
     if step == 1:
         meme_tempaltes()
     if step == 2:
         meme_editing()
     if step == 3:
         generate_meme_view()
+    if step == 4:
+        vibe_search()
+
+def view_select():
+    option = st.selectbox(label='Select Mode',
+        options=['Trending/Context','Vibe', 'Collaboration'])
+    if st.button('Go', type='primary'):
+        if option == 'Trending/Context':
+            st.session_state.step = 1
+        elif option == 'Vibe':
+            st.session_state.step = 4
+        else:
+            st.session_state.step = 6
+        st.rerun()
 
 def meme_tempaltes():
     st.header("Select Template")
@@ -78,6 +101,9 @@ def meme_tempaltes():
             st.session_state.step = 2
             st.session_state.mode = "context"
             st.rerun()
+    if st.button('Back'):
+        st.session_state.step = 0
+        st.rerun()
 
 def meme_editing():
     st.header("Edit Your Meme Text")
@@ -107,12 +133,12 @@ def meme_editing():
             submit_button = st.form_submit_button("Generate", type='primary')
                 
         if submit_button:
-                if len(form_top_text) < 1 or len(form_bottom_text) < 1:
-                    st.toast('Please input text')
-                else:
-                    set_text(form_top_text, form_bottom_text)
-                    st.session_state.step = 3
-                    st.rerun()
+            if len(form_top_text) < 1 or len(form_bottom_text) < 1:
+                st.toast('Please input text')
+            else:
+                set_text(form_top_text, form_bottom_text)
+                st.session_state.step = 3
+                st.rerun()
 
     if st.button("Back"):
         st.session_state.step = 1
@@ -142,8 +168,13 @@ def generate_meme_view():
 
     with col11:
         if st.button("Publish", type='primary'):
-            st.success("Meme Published!")
-
+            st.toast("Meme published!")
+            st.balloons()
+            st.session_state.tempalte = ''
+            st.session_state.step = 0
+            time.sleep(1.5)
+            st.rerun()
+            
 # Function to generate meme with text
 from PIL import Image, ImageDraw, ImageFont
 
@@ -198,19 +229,160 @@ def generate_meme(template_path, top_text, bottom_text):
 
     return image
 
-#def generate_meme(template_path, top_text, bottom_text):
-#    image = load_image(template_path)
-#    draw = ImageDraw.Draw(image)
-#    font = ImageFont.truetype("arial.ttf", 40)
+def vibe_search():
+    st.header('Search by Vibe')
 
-#    # Add top text
-#    draw.text((10, 10), top_text, font=font, fill="white")
+    with st.form(key='vibe_search'):
+        vibe_input = st.text_input(label='Vibe', value='exciting', placeholder='sad')
+        submit_button = st.form_submit_button(label='Search', type='primary')
+    
+        if submit_button:
+            if len(vibe_input) < 1:
+                st.toast('Please input text')
+            else:
+                st.session_state.vibe = vibe_input
+                st.rerun()
+    
+    if st.session_state.vibe == 'exciting':
+        memes = ['img/vibe/exciting/exciting1.jpg',
+        'img/vibe/exciting/exciting2.jpg',
+        'img/vibe/exciting/exciting3.jpg']
+    elif st.session_state.vibe == 'sad':
+        memes = ['img/vibe/sad/sad1.jpg',
+        'img/vibe/sad/sad2.jpg',
+        'img/vibe/sad/sad3.jpg']
+    else:
+        memes = []
+        
+    if len(memes) > 0:
+        for meme in memes:
+            st.image(meme, use_column_width=True)
+            if st.button(os.path.basename(meme), key=meme):
+                st.session_state.template = meme
+                st.session_state.step = 5
+                st.rerun()
+            st.write('')
+    else:
+        if len(st.session_state.vibe)>0: 
+            st.write('No results found')
+    
+    if st.button('Back'):
+        if len(st.session_state.vibe) > 0:
+            st.session_state.step = 4
+            st.session_state.vibe = ''
+            st.rerun()
+        else:
+            st.session_state.step = 0
+        st.rerun()
 
-    # Add bottom text
-#    width, height = image.size
-#    draw.text((10, height - 50), bottom_text, font=font, fill="white")
+def exploded_view():
+    st.header('Preview')
+    st.image(st.session_state.template, use_column_width=True)
+    col20, col21 = st.columns([.1,.9])
+    with col20:
+        if st.button('Back'):
+            st.session_state.step = 4
+            st.rerun()
+    with col21:
+        if st.button('Publish', type='primary'):
+            st.toast('Meme published!')
+            st.balloons()
+            st.session_state.template = ''
+            st.session_state.step=0
+            time.sleep(1.5)
+            st.rerun()
 
-#    return image
+def collab_base():
+    st.header('Team Collaboration')
+    col30, col31 = st.columns([.7,.3])
+
+    with col30:
+        if len(st.session_state.template) > 0:
+            st.image(st.session_state.template, use_column_width=True)
+        else:
+            st.image('img/slide2.png', use_column_width='auto')
+    
+    with col31:
+        votes1 = [6,0,0,2]
+        votes2 = [0,3,3,1]
+        votes3 = [1,1,0,0]
+        if len(st.session_state.template) > 0:
+            display_comments(votes1, votes2, votes3)
+        else:
+            display_collab_memes(votes1, votes2, votes3)
+
+def display_collab_memes(votes1, votes2, votes3):
+    memes = ['img/collab/collab1.jpg',
+    'img/collab/collab2.jpg',
+    'img/collab/collab3.jpg',
+    ]
+
+    for meme in memes:
+        if meme == 'img/collab/collab1.jpg':
+            votes(votes1[0],votes1[1],votes1[2],votes1[3])
+        elif meme == 'img/collab/collab2.jpg':
+            votes(votes2[0],votes2[1],votes2[2],votes2[3])
+        elif meme == 'img/collab/collab3.jpg':
+            votes(votes3[0],votes3[1],votes3[2],votes3[3])
+        
+        st.image(meme, use_column_width=True)
+        if st.button(os.path.basename(meme), key=meme):
+            st.session_state.template = meme
+            st.rerun()
+        st.write('')
+    
+    if st.button('Back'):
+        st.session_state.step = 0
+        st.rerun()
+
+def display_comments(votes1, votes2, votes3):
+    if st.session_state.template == 'img/collab/collab1.jpg':
+        votes(votes1[0],votes1[1],votes1[2],votes1[3])
+        comment('Jim', 'good choice!', '👍')
+        comment('Dwight', 'This is exactly the kind of strategic thinking we need. Use it.', '👍')
+    elif st.session_state.template == 'img/collab/collab2.jpg':
+        votes(votes2[0],votes2[1],votes2[2],votes2[3])
+        comment('Toby', 'While this meme is clever, it might come across as unprofessional to some.', '🙁')
+    else:
+        votes(votes3[0],votes3[1],votes3[2],votes3[3])
+        st.write('No comments')
+
+    col40, col41 = st.columns([.4,.6])
+    with col40:
+        if st.button('Back'):
+            st.session_state.template  = ''
+            st.rerun()
+    with col41:
+        if st.button('Publish',type='primary'):
+            st.toast('Meme published!')
+            st.balloons()
+            st.session_state.template  = ''
+            st.session_state.step = 0
+            time.sleep(1.5)
+            st.rerun()
+
+def comment(username, body, icon):
+    with st.expander(label=username, expanded=True, icon=icon):
+        st.write(body)
+
+def votes(num_thumbs_up, num_thumbs_down,num_sad, num_comments):
+    a, b, c, d = st.columns(4)
+
+    with a:
+        st.write(num_thumbs_up)
+        st.write('👍')
+
+    with b:
+        st.write(num_thumbs_down)
+        st.write('👎')
+
+    with c:
+        st.write(num_sad)
+        st.write('🙁')
+    
+    with d:
+        st.write(num_comments)
+        st.write('💬')
 
 if __name__ == "__main__":
     main()
